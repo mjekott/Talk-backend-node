14f9f386f5fa92c5337ee85605a94c44c0360806
mjekott <ekottmfon@yahoo.com>
2021-09-11 20:24:22 +0100

    added routes for post controllers
    Signed-off-by: mjekott <ekottmfon@yahoo.com>

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('posts')
 export class PostsController {
   @Get('/')
-  getAll(): string {
+  getAllPost(): string {
     return 'get all posts';
+  }
+
+  @Get(':/postid')
+  getPostDetails(@Param() param): string {
+    return 'get post details' + param.postid;
+  }
+
+  @Post('/')
+  createNewPost(): string {
+    return `new post was created `;
+  }
+
+  @Delete('/:postid')
+  deletePost(@Param() param): string {
+    return 'delete postid';
+  }
+
+  @Put('/:postid/like')
+  likePost(@Param() param): string {
+    return 'liked post';
+  }
+
+  @Delete('/:postid/like')
+  unlikePost(@Param() param): string {
+    return 'unliked a post';
   }
 }
