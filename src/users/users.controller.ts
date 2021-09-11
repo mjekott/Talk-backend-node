cf4ff80f671bc415c8737748ed730927d08efe20
mjekott <ekottmfon@yahoo.com>
2021-09-11 20:03:54 +0100

    added routes for user controllers
    Signed-off-by: mjekott <ekottmfon@yahoo.com>

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/@:username')
+  getUserByUsername(@Param() param): string {
+    return `detail of username = ${param.username}`;
+  }
+
+  @Get('/:userid')
+  getUserByUserid(@Param() param): string {
+    return `details of user id = ${param.userid}`;
+  }
+
+  @Post('/')
+  createNewUser(): string {
+    return `created a user`;
+  }
+
+  @Patch('/:userid')
+  updateUserDetails() {
+    return `created a user`;
+  }
+
+  @Put('/:userid/follow')
+  followUser() {
+    return `created a user`;
+  }
+
+  @Delete('/:userid/follow')
+  unfollowUser() {
+    return `created a user`;
+  }
+
+  @Get('/:userid/followers')
+  getFollowersOfUser() {
+    return `created a user`;
+  }
+
+  @Put('/:userid/followees')
+  getFolloweesOfuser(): string {
+    return `created a user`;
+  }
+}
